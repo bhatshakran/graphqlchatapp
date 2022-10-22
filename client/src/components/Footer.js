05696e3a6bc3962fcf45f8a6dfdec9352da6ad51
bhatshakran <bhatshakran@gmail.com>
2022-10-22 12:31:11 +0530

    Made footer fully responsive

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,28 +2,28 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='bg-white pt-40 mt-12 flex flex-wrap justify-evenly  items-center gap-16 px-12 pb-24'>
-      <div className='w-1/3 sm:w-auto'>
+    <div className='bg-white pt-40 mt-12 flex flex-wrap justify-evenly   items-center gap-16 md:gap-8 px-12 pb-24'>
+      <div className='w-full text-center sm:w-1/3 md:w-1/5 '>
         <h3 className='font-acworth text-2xl'>Chatter</h3>
-        <p className='w-60'>
+        <p className=' text-center '>
           A messaging app for everyone to connect with their loved ones.
         </p>
       </div>
-      <div className='w-1/3 sm:w-auto'>
+      <div className='w-full text-center sm:w-1/3 md:w-1/5'>
         <h2 className='font-bold'>Product</h2>
         <ul>
           <li>Uploads</li>
           <li>Downloads</li>
         </ul>
       </div>
-      <div className='w-1/3 sm:w-auto'>
+      <div className='w-full text-center sm:w-1/3 md:w-1/5'>
         <h2 className='font-bold'>Support</h2>
         <ul>
           <li>Help center</li>
           <li>Account information</li>
         </ul>
       </div>
-      <div className='w-1/3 sm:w-auto'>
+      <div className='w-full text-center sm:w-1/3 md:w-1/5'>
         <h2 className='font-bold'>Help Solutions</h2>
 
         <ul>
